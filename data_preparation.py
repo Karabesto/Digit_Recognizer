48ce59cc1b0ee9f4df7c931cd9066a299e9b4c69
Karabesto <you@example.com>
2023-10-13 11:06:33 +0200

    Work on labels

--- data_preparation.py
+++ data_preparation.py
@@ -6,25 +6,55 @@ import matplotlib.cm as cm
 
 
 class DataManipulation:
-    @staticmethod
-    def read_data():
+
+    def __init__(self):
+        self.data_train = None
+        self.data_test = None
+        self.images_train = None
+        self.images_test = None
+        self.labels_vector = None
+
+    def read_data(self):
+        """
+        Load the data from the dataset.
+        :return: Return an array with the first element being the dataset for training
+                    and the second element the test's one
+        """
         data_train = pd.read_csv('dataset/file_train.csv')
         data_test = pd.read_csv('dataset/file_test.csv')
+        self.data_train = data_train
+        self.data_test = data_test
         return data_train, data_test
 
-    @staticmethod
-    def prep_data(data):
+    def prep_data(self):
+        """
+        Convert the data loaded in data used for learning (aka images on grayscale).
+        :return: Return an array with the first element being the image for training
+                    and the second element the test's one
+        """
         # Each line is an image
-        images = data.iloc[:, 1:].values
-        print(images[0])
+        images = self.data_train.iloc[:, 1:].values
         # We force the type from int to float for each image
         images = images.astype(np.float64)
         # The data are encoded from 0 to 255 and we convert them to 0.0-1.0
         images = np.multiply(images, 1.0 / 255.0)
-        return images
+        self.images_test = images
+
+        # Each line is an image
+        images = self.data_test.iloc[:, 1:].values
+        # We force the type from int to float for each image
+        images = images.astype(np.float64)
+        # The data are encoded from 0 to 255 and we convert them to 0.0-1.0
+        images = np.multiply(images, 1.0 / 255.0)
+        self.images_test = images
+        return self.images_train, self.images_test
 
     @staticmethod
     def display_image(image):
+        """
+        Display an image given in parameter.
+        :param image: image to plot
+        """
         # The image size is the length of the row
         image_size = image.shape[0]
         # We define the height and width of the image with a square root because the images of the dataset are square
@@ -38,7 +68,27 @@ class DataManipulation:
         plt.imshow(img, cmap=cm.binary)
         plt.show()
 
-    @staticmethod
-    def numeric_2_vectors_labels(dataset):
-        print(dataset.loc[0])
+    def digit_2_vectors_labels(self, dataset):
+        """
+        Convert a digit to a vectors used later,
+        example : 0 -> [1 0 0 0 0 0 0 0 0 0],
+                  1 -> [0 1 0 0 0 0 0 0 0 0],...
+                  8 -> [0 0 0 0 0 0 0 0 1 0],...
+        :param dataset:
+        :return:
+        """
+        labels = dataset["label"]
+        labels_vector = np.zeros((len(labels), 10))  # length of labels and 10 digits (0..9)
+        for row_index, value in enumerate(labels):
+            labels_vector[row_index, value] = 1
+        self.labels_vector = labels_vector
+        return labels_vector
 
+    @staticmethod
+    def vectors_2_digit_labels(vectors_labels):
+        # Find the maximum value in each row
+        if vectors_labels.ndim == 1:
+            digit = np.argmax(vectors_labels)
+        else:
+            digit = np.argmax(vectors_labels, axis=1)
+        return digit
--- main.py
+++ main.py
@@ -4,8 +4,10 @@ data_manip = DataManipulation()
 # We read the dataset
 dataset_train, dataset_test = data_manip.read_data()
 # We prepare the data used for the training
-images = data_manip.prep_data(dataset_train)
+images_train, image_test = data_manip.prep_data()
 # We plot an image number_to_plot
 number_to_plot = 10
-# data_manip.display_image(images[number_to_plot])
-data_manip.numeric_2_vectors_labels(dataset_train)
+# data_manip.display_image(images_train[number_to_plot])
+labels_vect = data_manip.digit_2_vectors_labels(dataset_train)
+labels = data_manip.vectors_2_digit_labels(labels_vect)
+labels = data_manip.vectors_2_digit_labels(labels_vect[0])
